3979a3eaab764bccda0de519dfbf74314f839e2d
yisonPylkita <yisonPylkita@gmail.com>
2023-02-18 00:27:45 +0100

    version and verack messages serialization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod protocol;
 
-// fn connect_to_node(node_addr: String) ->
-
 fn main() {
     println!("Hello, world!");
 }
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,5 +1,6 @@
+use bitcoin::hashes::sha256d;
 // https://en.bitcoin.it/wiki/Protocol_documentation
-
+// TODO: I've decided to focus on version == 60002, support for other versions is missing
 use bytes::BytesMut;
 use std::convert::TryInto;
 use tokio_util::codec::{Decoder, Encoder};
@@ -29,7 +30,7 @@ struct VersionFrame {
     nonce: u64,
     user_agent: String,
     start_height: i32,
-    relay: bool,
+    // relay: bool,
 }
 
 #[derive(PartialEq, Debug)]
@@ -41,7 +42,12 @@ struct NetAddrVersion {
 
 impl NetAddrVersion {
     fn encode(&self) -> Vec<u8> {
-        unimplemented!();
+        let mut dst = Vec::with_capacity(26);
+        dst.extend_from_slice(&self.services.to_le_bytes());
+        dst.extend_from_slice(&self.ip_v6_v4);
+        dst.extend_from_slice(&self.port.to_le_bytes());
+
+        dst
     }
 
     fn decode(src: &[u8; 26]) -> Result<NetAddrVersion, std::io::Error> {
@@ -59,7 +65,20 @@ impl NetAddrVersion {
 
 impl VersionFrame {
     fn encode(&self) -> Vec<u8> {
-        unimplemented!();
+        let mut dst = Vec::with_capacity(26);
+        dst.extend_from_slice(&self.version.to_le_bytes());
+        dst.extend_from_slice(&self.services.to_le_bytes());
+        dst.extend_from_slice(&self.timestamp.to_le_bytes());
+        dst.extend_from_slice(&self.addr_recv.encode());
+        dst.extend_from_slice(&self.addr_from.encode());
+        dst.extend_from_slice(&self.nonce.to_le_bytes());
+        let user_agent_len = self.user_agent.len() as u8;
+        dst.extend_from_slice(&user_agent_len.to_le_bytes());
+        dst.extend_from_slice(&self.user_agent.as_bytes());
+        dst.extend_from_slice(&self.start_height.to_le_bytes());
+        // dst.extend_from_slice(&self.relay.to_le_bytes());
+
+        dst
     }
 
     fn decode(src: &mut BytesMut) -> Result<VersionFrame, std::io::Error> {
@@ -89,11 +108,11 @@ impl VersionFrame {
                 .unwrap(),
         );
         // TODO: relay only in version ≥ 70001
-        let relay = u8::from_le_bytes(
-            src[83 + user_agent_size..83 + user_agent_size + 1]
-                .try_into()
-                .unwrap(),
-        ) == 0x00;
+        // let relay = u8::from_le_bytes(
+        //     src[83 + user_agent_size..83 + user_agent_size + 1]
+        //         .try_into()
+        //         .unwrap(),
+        // ) == 0x00;
 
         Ok(VersionFrame {
             version,
@@ -104,7 +123,7 @@ impl VersionFrame {
             nonce,
             user_agent,
             start_height,
-            relay,
+            // relay,
         })
     }
 }
@@ -118,7 +137,13 @@ impl VerAckFrame {
     }
 
     fn decode(src: &mut BytesMut) -> Result<VerAckFrame, std::io::Error> {
-        // TODO: ensure src.len() == 0
+        if src.len() != 0 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Invalid size"),
+            ));
+        }
+
         Ok(VerAckFrame {})
     }
 }
@@ -135,9 +160,10 @@ impl Encoder<BaseFrame> for Codec {
             InternalFrame::Version(frame) => frame.encode(),
             InternalFrame::VerAck(frame) => frame.encode(),
         };
-        dst.extend_from_slice(&payload_bytes.len().to_le_bytes());
-        let checksum: u32 = 0x00; // TODO: implement checksum calculation
-        dst.extend_from_slice(&checksum.to_le_bytes());
+        let payload_size = payload_bytes.len() as u32;
+        dst.extend_from_slice(&payload_size.to_le_bytes());
+        let checksum = calculate_checksum(&payload_bytes[..]);
+        dst.extend_from_slice(&checksum);
         dst.extend_from_slice(&payload_bytes);
 
         Ok(())
@@ -169,6 +195,7 @@ impl Decoder for Codec {
         let checksum = u32::from_le_bytes(src[20..24].try_into().unwrap());
         let internal_payload = &src[24..24 + length as usize];
         println!("Command: {command}");
+        // TODO: check if checksum from received message is the same as calculated one
         let internal_frame = match command {
             "version" => InternalFrame::Version(
                 VersionFrame::decode(&mut BytesMut::from(internal_payload)).unwrap(),
@@ -198,12 +225,24 @@ fn str_to_padded_bytes(data: &str) -> Vec<u8> {
     if data.len() >= 12 {
         panic!("Buffer too large");
     }
-    let mut result: [u8; 12] = [0; 12];
-    for idx in 0..11 {
+    let mut result = vec![0x00; 12];
+    for idx in 0..data.len() {
         result[idx] = data.as_bytes()[idx];
     }
 
-    Vec::from(result)
+    result
+}
+
+fn calculate_checksum(data: &[u8]) -> [u8; 4] {
+    use bitcoin_hashes::sha256;
+    use bitcoin_hashes::Hash;
+
+    let mut reader: &[u8] = data;
+    let mut engine = sha256::HashEngine::default();
+    std::io::copy(&mut reader, &mut engine).unwrap();
+    let hash = sha256d::Hash::from_engine(engine);
+
+    [hash[0], hash[1], hash[2], hash[3]]
 }
 
 #[cfg(test)]
@@ -262,7 +301,7 @@ mod test {
                     nonce: 7284544412836900411,
                     user_agent: "/Satoshi:0.7.2/".to_string(),
                     start_height: 212672,
-                    relay: false,
+                    // relay: false,
                 })
             }
         );
@@ -288,5 +327,90 @@ mod test {
                 internal_frame: InternalFrame::VerAck(VerAckFrame {})
             }
         );
+    }
+
+    #[test]
+    fn encode_version_frame() {
+        let mut codec = Codec {};
+        let mut encoded_frame = BytesMut::new();
+        codec
+            .encode(
+                BaseFrame {
+                    magic: 0xd9b4bef9,
+                    command: "version".to_string(),
+                    length: 100,
+                    checksum: 0x32498d35,
+                    internal_frame: InternalFrame::Version(VersionFrame {
+                        version: 60002,
+                        services: 1,
+                        timestamp: 1355854353,
+                        addr_recv: NetAddrVersion {
+                            services: 1,
+                            ip_v6_v4: vec![
+                                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff,
+                                0xff, 0x00, 0x00, 0x00, 0x00,
+                            ]
+                            .try_into()
+                            .unwrap(),
+                            port: 0,
+                        },
+                        addr_from: NetAddrVersion {
+                            services: 0,
+                            ip_v6_v4: vec![
+                                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff,
+                                0xff, 0x00, 0x00, 0x00, 0x00,
+                            ]
+                            .try_into()
+                            .unwrap(),
+                            port: 0,
+                        },
+                        nonce: 7284544412836900411,
+                        user_agent: "/Satoshi:0.7.2/".to_string(),
+                        start_height: 212672,
+                        // relay: false,
+                    }),
+                },
+                &mut encoded_frame,
+            )
+            .unwrap();
+        let expected_frame = BytesMut::from(
+            &[
+                0xf9, 0xbe, 0xb4, 0xd9, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x64, 0x00, 0x00, 0x00, 0x35, 0x8d, 0x49, 0x32, 0x62, 0xea, 0x00, 0x00,
+                0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11, 0xb2, 0xd0, 0x50, 0x00, 0x00,
+                0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3b, 0x2e,
+                0xb3, 0x5d, 0x8c, 0xe6, 0x17, 0x65, 0x0f, 0x2f, 0x53, 0x61, 0x74, 0x6f, 0x73, 0x68,
+                0x69, 0x3a, 0x30, 0x2e, 0x37, 0x2e, 0x32, 0x2f, 0xc0, 0x3e, 0x03, 0x00,
+            ][..],
+        );
+        assert_eq!(hex::encode(expected_frame), hex::encode(encoded_frame));
+    }
+
+    #[test]
+    fn encode_verack_frame() {
+        let mut codec = Codec {};
+        let mut encoded_frame = BytesMut::new();
+        codec
+            .encode(
+                BaseFrame {
+                    magic: 0xd9b4bef9,
+                    command: "verack".to_string(),
+                    length: 0,
+                    checksum: 0xe2e0f65d,
+                    internal_frame: InternalFrame::VerAck(VerAckFrame {}),
+                },
+                &mut encoded_frame,
+            )
+            .unwrap();
+        let expected_frame = BytesMut::from(
+            &[
+                0xF9, 0xBE, 0xB4, 0xD9, 0x76, 0x65, 0x72, 0x61, 0x63, 0x6B, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5D, 0xF6, 0xE0, 0xE2,
+            ][..],
+        );
+        assert_eq!(hex::encode(expected_frame), hex::encode(encoded_frame));
     }
 }
